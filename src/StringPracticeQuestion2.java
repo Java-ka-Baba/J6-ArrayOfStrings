d23770dc724ba914f51a08870bc6ff4df44273dd
Java ka Baba <divaibhav.training@gmail.com>
2024-09-10 20:10:48 +0530

    solution

--- src/StringPracticeQuestion2.java
+++ src/StringPracticeQuestion2.java
@@ -13,6 +13,33 @@ public class StringPracticeQuestion2 {
                "Dharam singh"};
     */
     public static void main(String[] args) {
-
+        String[] names = {"Piyush Sahu", "Harish Chourasiya", "Anjali Prajapati"
+                , "Gauri Dahake",
+                "Ankita Bobde", "Shiddharth Patel",
+                "Madhur Sahu", "Aaditya Rajawat", "Akshay Mishra", "Devansh Sharma",
+                "Kanishka singh kushwaha", "Anushka Shrivastava",
+                "Devanshi Choudhary",
+                "Devanshi bamaliya", "Divyansh Malviya", "Adarsh Tiwari",
+                "Aatmik kushwaha", "Ankit Gujre",
+                "shanawaz alam", "Raman Jatav", "Aditya Tanwani", "Mohd Nadir Ansari", "Deepika Patel", "Megha thakre",
+                "Dharam singh" };
+        int count = 0;
+        for (String element : names) {
+            if (element.startsWith("a") ||
+                    element.startsWith("e") ||
+                    element.startsWith("i") ||
+                    element.startsWith("o") ||
+                    element.startsWith("u") ||
+                    element.startsWith("A") ||
+                    element.startsWith("E") ||
+                    element.startsWith("I") ||
+                    element.startsWith("O") ||
+                    element.startsWith("U")
+            ) {
+                count++;
+                System.out.println(element);
+            }
+        }
+        System.out.println("count = " + count);
     }
 }
