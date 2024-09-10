f27992f903ebff35388d20124aadca50b56a7e2c
Java ka Baba <divaibhav.training@gmail.com>
2024-09-10 20:05:06 +0530

    Question

---
+++ src/StringPracticeQuestion2.java
@@ -0,0 +1,18 @@
+public class StringPracticeQuestion2 {
+    /*
+   WAP to count all the Strings those are starting with vowels from the given
+    String[] names = {"Piyush Sahu","Harish Chourasiya","Anjali Prajapati"
+               ,"Gauri Dahake",
+               "Ankita Bobde","Shiddharth Patel",
+               "Madhur Sahu","Aaditya Rajawat","Akshay Mishra","Devansh Sharma",
+               "Kanishka singh kushwaha","Anushka Shrivastava",
+               "Devanshi Choudhary",
+               "Devanshi bamaliya","Divyansh Malviya","Adarsh Tiwari",
+               "Aatmik kushwaha","Ankit Gujre",
+               "shanawaz alam","Raman Jatav","Aditya Tanwani","Mohd Nadir Ansari","Deepika Patel","Megha thakre",
+               "Dharam singh"};
+    */
+    public static void main(String[] args) {
+
+    }
+}
