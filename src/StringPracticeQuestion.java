4b680c5b63e7f0b8f13153e984726c50dbe53876
Java ka Baba <divaibhav.training@gmail.com>
2024-09-10 19:38:02 +0530

    Question

---
+++ src/StringPracticeQuestion.java
@@ -0,0 +1,7 @@
+public class StringPracticeQuestion {
+    /*
+    WAP to find the smallest String from the given
+    String[] names = {Piyush Sahu,Harish Chourasiya,Anjali Prajapati,Gauri Dahake,Ankita Bobde,Shiddharth Patel,Madhur Sahu,Aaditya Rajawat,Akshay Mishra,Devansh Sharma,Kanishka singh kushwaha,Anushka Shrivastava,Devanshi Choudhary,Devanshi bamaliya,Divyansh Malviya,Adarsh Tiwari,Aatmik kushwaha,Ankit Gujre,shanawaz alam,Raman Jatav,Aditya Tanwani,Mohd Nadir Ansari,Deepika Patel,Megha thakre,Dharam singh};
+    A String is said to be small based on its length
+     */
+}
